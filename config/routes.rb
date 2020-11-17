ead75a083350199249162010cb1eadd364b22c8b
Richard Schentag <richard@schentag.co>
2020-11-17 14:43:04 -0600

    added new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/show'
+  get 'home/new'
+  get 'home/create'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :home
+
+
+  root to: "home#index"
 end

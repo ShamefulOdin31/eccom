312fc6f3c8ddbe94ad25eb262fb25095e45ab795
Richard Schentag <richard@schentag.co>
2020-11-27 14:39:52 -0600

    added customer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_193222) do
+ActiveRecord::Schema.define(version: 2020_11_27_201942) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2020_11_17_193222) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "address"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "games", force: :cascade do |t|
     t.string "name"
     t.integer "category_id"
@@ -68,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_193222) do
     t.text "comments"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id"
   end
 
 end

b58c679d559c34a07ecc055150db5f7ee991df1d
Richard Schentag <richard@schentag.co>
2020-11-27 11:54:55 -0600

    added update

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -2,14 +2,20 @@ class ShoppingController < ApplicationController
   def index
     @cart = session[:cart]
     @products = []
-    @cart.each do |cart|
+    if @cart.nil?
+
+    else
+      @cart.each do |cart|
       total = cart['game']['price'].to_i * cart['qty'].to_i
 
       new_product = { "game" => cart['game'], "qty" => cart['qty'], "total" => total }
       @products.push(new_product)
     end
+    end
 
-
+    def update
+      @cart = session[:cart]
+    end
     
 
     
